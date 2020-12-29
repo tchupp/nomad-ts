4736912a3c27735857f229f5df473200b5e018e7
theochupp <tclchiam@gmail.com>
2020-12-29 13:18:25 -0500

    chore: enable power-assert for better test output

--- packages/core/ava.config.js
+++ packages/core/ava.config.js
@@ -1,11 +1,21 @@
 export default {
+    "babel": {
+        "extensions": ["ts"],
+        "testOptions": {
+            "presets": [
+                ["module:@ava/babel/stage-4", false]
+            ],
+            "plugins": [
+                "@babel/plugin-syntax-typescript",
+            ],
+        }
+    },
     "cache": true,
     "concurrency": 5,
     "timeout": "30s",
     "failFast": true,
     "verbose": true,
     "files": ["tests/**/*.test.ts"],
-    "extensions": ["ts"],
     "require": [
         "ts-node/register",
     ]
